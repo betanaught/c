82a864bde54216dee1f543ec6c8df3ab68cc40c2
Brendan Wakefield <22670871+betanot@users.noreply.github.com>
2020-01-02 09:48:00 -0800

    Added first function (pg. 24) for rasing base to nth power

---
+++ function1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* POWER function(a,b) {return a^b} for a>=0 */
+int power(int base, int n);
+
+int main() {
+  int i, p;
+  p = 1;
+  for (i = 1; i <= n; ++i)
+    p = p * base;
+  return p;
+}
