4de25412b7d917f1c19ec284e102d08b04a44cce
Brendan Wakefield <22670871+betanot@users.noreply.github.com>
2020-01-21 22:12:04 -0800

    Re-creation of array1.c

--- array1.c
+++ array1.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 
-/* Count digits, white space, and other characters from input */
-int main() {
+/* code to count digits, white space, and other chars */
+int main(){
   int c, i, nwhite, nother;
   int ndigit[10];
 
   nwhite = nother = 0;
-  for (i = 0; i < 10; ++i)
+  for  (i = 0; i < 10; ++i)
     ndigit[i] = 0;
 
   while ((c = getchar()) != EOF)
-    if (c >= '0' && c <= '9')
+    if ( >='0' && c <= '9')
       ++ndigit[c - '0'];
     else if (c == ' ' || c == '\n' || c == '\t')
       ++nwhite;
-      else
+    else
       ++nother;
 
-  printf("counts:  0 1 2 3 4 5 6 7 8 9\n");
-  printf("digits =");
+  printf("digits = ");
   for (i = 0; i < 10; ++i)
-    printf(" %d", ndigit[i]);
-  printf(", white space = %d, other = %d\n",
-	 nwhite, nother);
+    printf(" %d", ndigit[1]);
+  printf(", white space = %d, other = %d\n", nwhite, nother);
 }
