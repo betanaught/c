31d4e8e563aa71d49c4264bd065ace021e5cb15c
betanot <betanot@github.com>
2020-05-18 20:41:33 -0700

    Updated notes on collapsed Winter Pi digits c program; removed binary executable files accidentally added earlier

--- pi-digits-winter-expand.c
+++ pi-digits-winter-expand.c
@@ -6,6 +6,16 @@ f[b]=d%--g,d/=g--,--b;d*=b);}
 /*
 /* Expanded code: */
 
+/* Some helpful explanations:
+   int l = --b --> b = b-1; l = b;
+   int l = b-- --> l = b; b-1;
+   d *= b --> d = d*b;
+   d /= g-- --> d = d/g; g = g-1
+   f[b] = d % --g --> g = g-1; f[b] = d % g;
+   d += f[b] * a --> d = (f[b]*a) + d
+
+*/
+
 int a = 10000, b, c = 2800, d, e, f[2801], g;
 
 main() {
